64114930fd6917b81d8acff2931609f82524c9ef
Lalit Kumar <103807663+Lalitcodec@users.noreply.github.com>
2025-03-02 13:10:13 +0000

    utils

--- src/app.js
+++ src/app.js
@@ -12,7 +12,11 @@ app.use(cors({
 }))
 
 app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({extended : true, limit : "16kb"}))
+
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 
 export {app};
